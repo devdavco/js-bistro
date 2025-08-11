6fe891ffdd6178d820feeb98b0cb195bb398e9ad
David Corrales <github.ferocity308@passmail.com>
2025-08-11 17:13:48 -0500

    Sacando total por cliente

--- app.js
+++ app.js
@@ -2,23 +2,23 @@
 //menu y platosCrudos
 
 const menu = [
-    { id: 1, nombre: "Fideos a la Boloñesa", categoria: "Pasta", precio: 24000, descripcion: "Pasta fresca con salsa de tomate y carne molida" },
-    { id: 2, nombre: "Pizza Margarita", categoria: "Pizza", precio: 25000, descripcion: "Masa artesanal con salsa de tomate, mozzarella y albahaca" },
-    { id: 3, nombre: "Ensalada César", categoria: "Ensaladas", precio: 20000, descripcion: "Lechuga romana, pollo a la plancha, crutones y aderezo César" },
-    { id: 4, nombre: "Lasaña de Pollo", categoria: "Pasta", precio: 25500, descripcion: "Capas de pasta, pollo desmechado y salsa bechamel" },
-    { id: 5, nombre: "Sopa de Verduras", categoria: "Sopas", precio: 12500, descripcion: "Caldo casero con variedad de vegetales frescos" }
+    { id: 0, nombre: "Fideos a la Boloñesa", categoria: "Pasta", precio: 24000, descripcion: "Pasta fresca con salsa de tomate y carne molida" },
+    { id: 1, nombre: "Pizza Margarita", categoria: "Pizza", precio: 25000, descripcion: "Masa artesanal con salsa de tomate, mozzarella y albahaca" },
+    { id: 2, nombre: "Ensalada César", categoria: "Ensaladas", precio: 20000, descripcion: "Lechuga romana, pollo a la plancha, crutones y aderezo César" },
+    { id: 3, nombre: "Lasaña de Pollo", categoria: "Pasta", precio: 25500, descripcion: "Capas de pasta, pollo desmechado y salsa bechamel" },
+    { id: 4, nombre: "Sopa de Verduras", categoria: "Sopas", precio: 12500, descripcion: "Caldo casero con variedad de vegetales frescos" }
 ];
 
 const pedidosCrudos = [
     ["Cliente 1", 1, 3],
-    [null, 2, 5],
-    ["Cliente 3", 5, 5, 5],
+    [null, 2, 4],
+    ["Cliente 3", 4, 4, 4],
     [undefined, 1],
-    ["Cliente 4", 4, 2],
-    [null, 3, 1, 5],
-    ["Cliente 6", 2, 2, 2],
-    [undefined, 4, 5],
-    ["Cliente 8", 3],
+    ["Cliente 5", 4, 2],
+    [null, 3, 1, 4],
+    ["Cliente 7", 2, 2, 2],
+    [undefined, 4, 4],
+    ["Cliente 9", 3],
     [null, 1, 4]
 ];
 
@@ -26,15 +26,15 @@ const pedidosCrudos = [
 function procesarPedido(pedido){
 
     let pedidoModificado= pedido.slice() //Hacemos una copia del array
-    let bebida = true;
     let nombreCliente = pedidoModificado.shift(); //extraemos nombre del cliente
     
     if(nombreCliente === null || nombreCliente === undefined ){
         nombreCliente = "Cliente Anónimo"
     }
     pedidoModificado.push(nombreCliente) //Colocamos nombre del cliente al final
+    let bebida = true;
     pedidoModificado.unshift(bebida); //agregamos bebida al comienzo del array
-
+    
     return pedidoModificado
 }
 
@@ -49,15 +49,23 @@ function calcularIva(){
 
 //Proceso cada pedidoCrudo
 pedidosCrudos.forEach(pedidoSinProcesar => {
+
     let pedidoProcesado = procesarPedido(pedidoSinProcesar)
 
     let idsPlatosPedido = pedidoProcesado.slice(1,-1) //quito el comienzo y final del pedido procesado
-    console.log(idsPlatosPedido)
+    // console.log(idsPlatosPedido)
+    let totalPedido = 0
+
+    idsPlatosPedido.forEach(idplato => {
+        totalPedido = totalPedido + menu[idplato].precio
+    });
+
+    console.log("Total Pedido ",totalPedido,pedidoProcesado.pop() )
  // Aquí encontrar los ids de los platos en el array menu, sumar los precios 
 
 
     //lista de pedidos formateada.
-    console.log(pedidoProcesado)
+    // console.log(pedidoProcesado)
 
 });
 
